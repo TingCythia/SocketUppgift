58bc2abcbbcb58f3daeadb8a2d4ce0cfb79b41e7
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-09-03 21:35:34 +0200

    push server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import { createServer } from "http";
-import { express } from "express";
+import  express  from "express";
 import { Server } from "socket.io";
 
 const app = express()
 const httpServer= createServer(app);
 const port = 3001;
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+    cors: {
+      origin: '*',
+    }
+});
 
+httpServer.listen(port, () => {
+    console.log("Server is running on port " + port);
+})
