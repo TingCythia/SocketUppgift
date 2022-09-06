fe2b43b10466c465be2df539030e9fd9f18e6355
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-09-06 20:33:43 +0200

    room and nickname update

--- client/logic.js
+++ client/logic.js
@@ -2,20 +2,38 @@ const socket = io("http://localhost:3001")
 
 var form = document.getElementById("chat-form");
 var input = document.getElementById("msg");
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// get userroom and name from URL
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+const username= urlParams.get('username')
+console.log(username);
+let room = urlParams.get('room')
+console.log(room);
+
+// Join chatroom
+socket.emit('joinRoom', {username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 // Message from server
-socket.on('message', message=> {
-    console.log(message);
-    outputMessage(message);
-
+socket.on('message',(message)=> {
+    outputMessage( message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
   });
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    const message =input.value;
-        console.log(message)
-        outputMessage(`User: ${message}`)
-        socket.emit('chatMessages', message);
+    const msg =input.value;
+        socket.emit('chatMessages',msg);
         input.value = '';
 
 })
@@ -25,14 +43,29 @@ function outputMessage(message) {
 
     const div = document.createElement('div');
     div.classList.add('message');
-/*     const p = document.createElement('p');
+    const p = document.createElement('p');
     p.classList.add('meta');
-    p.innerText = message.username;
-    div.appendChild(p); */
+    p.innerText =message.username;
+    div.appendChild(p);  
     const para = document.createElement('p');
     para.classList.add('text');
-    para.innerText = message;
+    para.innerText = message.text;
     div.appendChild(para);
     document.querySelector('.chat-messages').append(div);
 
   }
+
+  // Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const port = 3001;
 
 
 app.use("/", express.static("./client"))
+
 /* app.get(`/:username`, async function (req, res) {
   const url =
       'https://localhost:3001/';
@@ -46,21 +47,90 @@ app.use("/", express.static("./client"))
   } 
 }); */
 
+// create user
+const users = [];
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+  users.push(user);
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
+// format username and message
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+  };
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+const appName = "ChatApp";
+
 io.on("connection", (socket) => {
     console.log("Connected!" + socket.id)
 
+    // join room
+    console.log(io.of("/").adapter);
+    socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
+    socket.join(user.room);  
+
+    // Broadcast when a user connects
+     io
+    .to(user.room)
+    .emit(
+      "message",
+      formatMessage(appName, `User ${user.username} has joined the chat`)
+    );
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  })
     //receive from client
     socket.on('chatMessages', (msg) =>{
-      console.log(msg);
-           // Welcome current user
-     socket.broadcast.emit("message", msg);
-    
+   
+      const user = getCurrentUser(socket.id);
+      console.log(user.username, msg);
+     //socket.broadcast.emit("message", msg);
+     io.in(user.room).emit("message", formatMessage(user.username, msg));
     })
-
+    
     // Disconnection
     socket.on('disconnect', () => {
-    console.log('user disconnected');
+      const user = userLeave(socket.id);
+
+      if (user) {
+        io.to(user.room).emit(
+          "message",
+          formatMessage(appName, `${user.username} has left the chat`)
+        );
+  
+        // Send users and room info
+        io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        });
+      }
   });
 })
 
