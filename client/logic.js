2457d0f49949f027cdef02dd96fa6f2a30be8d4e
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-09-06 11:08:12 +0200

    update message to render on DOM

--- client/logic.js
+++ client/logic.js
@@ -1,9 +1,38 @@
-
 const socket = io("http://localhost:3001")
 
+var form = document.getElementById("chat-form");
+var input = document.getElementById("msg");
 
-socket.on("connect", () => {
-const body = document.getElementById("body")
-body.innerHTML = "New socket connected with id: " + socket.id;
+// Message from server
+socket.on('message', message=> {
+    console.log(message);
+    outputMessage(message);
+
+  });
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const message =input.value;
+        console.log(message)
+        outputMessage(`User: ${message}`)
+        socket.emit('chatMessages', message);
+        input.value = '';
+
 })
 
+
+function outputMessage(message) {
+
+    const div = document.createElement('div');
+    div.classList.add('message');
+/*     const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    div.appendChild(p); */
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').append(div);
+
+  }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { createServer } from "http";
 import  express  from "express";
 import { Server } from "socket.io";
-
+import fetch from "node-fetch";
 
 
 const app = express()
@@ -18,12 +18,50 @@ const port = 3001;
 
 
 app.use("/", express.static("./client"))
+/* app.get(`/:username`, async function (req, res) {
+  const url =
+      'https://localhost:3001/';
+  const options = {
+      method: 'GET',
+      headers: {
+        "Content-Type" : "application/json"
+      }
+  };
+
+  // promise syntax
+ fetch(url, options)
+ .then(res => res.json())
+ .then(body=>username = body.username)
+ .then(()=>console.log(username))
+
+ .catch(err => console.error('error:' + err));  
+
+  try {
+      let response =  await fetch(url, options);
+      response = await response.json();
+      res.status(200).json(response);
+  } catch (err) {
+      console.log(err);
+      res.status(500).json({msg: `Internal Server Error.`});
+  } 
+}); */
 
 io.on("connection", (socket) => {
-    console.log(socket.id)
-    socket.on("connect", ()=>{
-        console.log()
+    console.log("Connected!" + socket.id)
+
+
+    //receive from client
+    socket.on('chatMessages', (msg) =>{
+      console.log(msg);
+           // Welcome current user
+     socket.broadcast.emit("message", msg);
+    
     })
+
+    // Disconnection
+    socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 })
 
 httpServer.listen(port, () => {
